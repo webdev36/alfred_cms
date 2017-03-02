3d75cea5a841e3dbf3ce9b9541a55f8efcc0b79c
webmaster219@yandex.com <admin@admins-Mac-9.local>
2017-03-02 09:14:37 -0800

    add privacy and other pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def privacy_policy
+  end
+
+  def faq
+  end
+
+  def code_of_conduct
+  end
+
+  def terms_and_conditions
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'home/temp'
-  
+
+  get 'home/privacy_policy'
+  get 'home/faq'
+  get 'home/code_of_conduct'
+  get 'home/terms_and_conditions'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
